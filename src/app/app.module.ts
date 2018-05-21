13e4a25f7741a10aac14aa0a5606e64fa77adc67
Akshay Sarkar <akshay.sarkar.dbit@gmail.com>
2018-05-20 23:59:34 -0500

    Login/Account Menu
    Form Validation
    Hide/Show Menu as per Status

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { MovieComponent } from './movie/movie.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    MovieComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  formData = {email: '', password: ''};
+  submitted = false;
+  constructor(private router: Router) {}
+  ngOnInit() {}
 
-  ngOnInit() {
+  onSubmit() {
+    console.log('Here');
+    this.submitted = true;
+    /*this.router.navigate(['/movie']);*/
   }
 
 }
